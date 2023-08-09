a61039af70814daebb0dee52944b6fb5a94fc57f
Aleksandr Dukhovnyy <script@vip-person.net>
2023-08-09 13:09:48 +0300

    util 'random' updated

--- utils/random.js
+++ utils/random.js
@@ -11,11 +11,67 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(randomNumber * (max - min + 1)) + min;
 }
 
+const randomLorem = (() => {
+  function random(param) {
+    if (typeof param === 'number') return Math.floor(Math.random() * param);
+
+    return param[random(param.length)];
+  }
+
+  return (wordsCount = 20) => {
+    const words = [
+      'consectetur',
+      'adipisicing',
+      'elit.',
+      'Officiis,',
+      'impedit',
+      'quia?',
+      'Asperiores',
+      'nisi',
+      'molestias',
+      'explicabo',
+      'labore,',
+      'consectetur',
+      'minus',
+      'repellat',
+      'voluptate',
+      'excepturi',
+      'cupiditate',
+      'nam',
+      'atque',
+      'facilis',
+      'tempore',
+      'soluta',
+      'minima',
+      'mollitia',
+      'neque',
+      'sit',
+      'amet',
+    ];
+
+    return (
+      'Lorem ipsum dolor ' +
+      Array.from({ length: random(wordsCount - 3) }, () => random(words)).join(
+        ' '
+      ) +
+      random(['.', '?', '!', '...'])
+    );
+  };
+})();
+
 export default {
-  util({ log }, [length = '16', radix = '32', useRandomCases = '1']) {
+  util({ log, flags }, [length = '16', radix = '32', useRandomCases = '1']) {
     length = +length;
     radix = +radix;
     useRandomCases = useRandomCases === '1';
+
+    if (length > 0 && (flags.hasFlag('lorem') || flags.hasFlag('l'))) {
+      log({
+        text: `random lorem string: {color: #53fff5 | ${randomLorem(length)}}`,
+      });
+
+      return;
+    }
 
     const res = Array.from({ length }, () => {
       const num = getRandomIntInclusive(0, radix).toString(radix);
