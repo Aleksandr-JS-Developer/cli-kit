cdb2859c9b2c4872cc26b9fefa1dcf9213c099e8
aleksandr-js-developer <script@vip-person.net>
2023-11-29 21:52:28 +0200

    added 'ping' tool

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import hex from './utils/hexToRadix.js';
 import random from './utils/random.js';
 import crypto from './utils/crypto.js';
 import sum from './utils/sum.js';
+import ping from './utils/ping.js';
 
 const main = () => {
   if (document.readyState === 'loading') {
@@ -23,12 +24,16 @@ const main = () => {
       c: crypto,
       sum,
       s: sum,
+      ping,
+      p: ping,
     },
     {
       fillParent: true,
       exec: true,
     }
   );
+
+  term.exec('p  --http');
 
   term.config.input.showInput = true;
 };
---
+++ utils/ping.js
@@ -0,0 +1,91 @@
+import PurifyHTML from 'https://cdn.jsdelivr.net/npm/purify-html/+esm';
+
+const sanitizer = new PurifyHTML.default();
+
+export default {
+  util({ log, flags }, [host]) {
+    const method = (flags.getFlag('method')?.value || 'GET').toUpperCase();
+    const url = new URL(
+      `${flags.hasFlag('http') ? 'http' : 'https'}://${
+        host === '/' ? '' : host
+      }`
+    );
+
+    // Cors
+    const originsIsMatched = url.origin === location.origin;
+    const corsMode =
+      flags.getFlag('cors')?.value || originsIsMatched ? 'cors' : 'no-cors';
+
+    const body = flags.hasFlag('body')
+      ? flags.getFlag('body')?.value
+      : undefined;
+
+    log({
+      text: `{color: #89fff1 | ${method} ${url.href}}`,
+    });
+
+    try {
+      fetch(url, {
+        method,
+        body,
+        mode: corsMode,
+      })
+        .then((res) => {
+          let color = '';
+
+          switch (true) {
+            case res.status < 200:
+              color = '#eee';
+              break;
+
+            case res.status < 300:
+              color = '#57fc57';
+              break;
+
+            case res.status < 400:
+              color = '#f0fb54';
+              break;
+
+            case res.status < 500:
+              color = '#fb5b5b';
+              break;
+
+            default:
+              color = '#ff0000';
+          }
+
+          log({
+            text: `{color: ${color} | ${res.status}: ${res.statusText}}`,
+          });
+
+          return res.text();
+        })
+        .then((res) => {
+          log({
+            text: sanitizer.toHTMLEntities(res),
+          });
+        })
+        .catch((err) => {
+          const isCorsError = !originsIsMatched && corsMode === 'cors';
+
+          log({
+            text: `${err.message}${isCorsError ? ': CORS error' : ''}`,
+            styles: {
+              color: 'red',
+            },
+          });
+        });
+    } catch (err) {
+      log({
+        text: err.message,
+        styles: {
+          color: 'red',
+        },
+      });
+    }
+  },
+  scheme: {
+    description:
+      'Executes mathematical expressions using eval, after removing all unnecessary symbols',
+  },
+};
